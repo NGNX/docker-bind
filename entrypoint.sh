88520c32dd56d62ecba1eec754a62a909532b09e
Rajib Mazumder <razib@me.com>
2019-07-17 18:05:47 +0600

    drop webmin

--- entrypoint.sh
+++ entrypoint.sh
@@ -51,19 +51,6 @@ create_bind_data_dir() {
   ln -sf ${BIND_DATA_DIR}/lib /var/lib/bind
 }
 
-create_webmin_data_dir() {
-  mkdir -p ${WEBMIN_DATA_DIR}
-  chmod -R 0755 ${WEBMIN_DATA_DIR}
-  chown -R root:root ${WEBMIN_DATA_DIR}
-
-  # populate the default webmin configuration if it does not exist
-  if [ ! -d ${WEBMIN_DATA_DIR}/etc ]; then
-    mv /etc/webmin ${WEBMIN_DATA_DIR}/etc
-  fi
-  rm -rf /etc/webmin
-  ln -sf ${WEBMIN_DATA_DIR}/etc /etc/webmin
-}
-
 set_root_passwd() {
   echo "root:$ROOT_PASSWORD" | chpasswd
 }
